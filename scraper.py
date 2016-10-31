e8a7c6be75ef91d13fc351b5db38c4bbb2813dec
Adrian David Smith <sierra.as74@gmail.com>
2016-10-31 15:34:37 +0200

    print

--- scraper.py
+++ scraper.py
@@ -47,12 +47,25 @@ class Scraper:
             for link in self.unparsed_links:
 
                 print "Parsing: " + link
-                data = urllib.urlopen(link)
 
-                htmlparser.feed(data.read())
-                htmlparser.close()
+                split = link.split("/")
+                # dont do a call if its an article slug
+                # slugs start with a date
+                if split[-1][0:4].isdigit() is False:
+                    data = urllib.urlopen(link)
+                    htmlparser.feed(data.read())
+                    htmlparser.close()
+                    self.current_links = htmlparser.get_links()
 
-                self.current_links = htmlparser.get_links()
+                    # link is subsection
+                    if len(split) == 5:
+                        print 'subsection'
+                    # link is section
+                    elif len(split) == 4:
+                        print 'seciton or publication section'
+
+
+
                 self.parsed_links.append(link)
                 self.unparsed_links.remove(link)
                 self.sort_links()
@@ -99,7 +112,7 @@ class Scraper:
         if link.startswith("http://") and link not in self.base_url in link:
             return None
 
-    def generate_map(self, output):
+    def generate_map(self):
 
         xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
         xml += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9\thttp://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">"
@@ -108,13 +121,20 @@ class Scraper:
 
         for link in self.parsed_links:
 
-            priority = 0.5
+            priority = 0.9
             split = link.split("/")
 
-            if utils.has_numbers(split[-1]) is True:
+            # link is article
+            if split[-1][0:4].isdigit() is True:
+                priority = 0.7
+            # link is subsection
+            elif len(split) == 5:
                 priority = 0.8
+            # link is section
+            elif len(split) == 4:
+                priority = 0.9
 
-            xml += "<url><loc>" + link + "</loc><changefreq>weekly</changefreq><priority>" + str(priority) + "</priority></url>"
+            xml += "<url><loc>" + link + "</loc><changefreq>daily</changefreq><priority>" + str(priority) + "</priority></url>"
 
         xml += "</urlset>"
 
@@ -132,6 +152,6 @@ class Utils:
 
 
 scraper = Scraper("http://www.wantedonline.co.za")
-xml = scraper.generate_map("out.xml")
+xml = scraper.generate_map()
 
 print xml
